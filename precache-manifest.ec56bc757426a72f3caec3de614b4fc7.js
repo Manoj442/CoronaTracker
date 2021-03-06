898100c4f82ce7534c482ea2f122975bd29f316b
Manoj442 <manoj.gudivada1@gmail.com>
2021-03-06 07:44:21 +0530

    Updates

---
+++ precache-manifest.ec56bc757426a72f3caec3de614b4fc7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "795ca458b4fac8612d69fd0b1595901d",
+    "url": "/CoronaTracker/index.html"
+  },
+  {
+    "revision": "d5293c636580b7c1f82d",
+    "url": "/CoronaTracker/static/css/main.3451145f.chunk.css"
+  },
+  {
+    "revision": "1f1a9c86b1ea161326b2",
+    "url": "/CoronaTracker/static/js/2.bbdff6c0.chunk.js"
+  },
+  {
+    "revision": "953ddbfd33e29cfe417b4c1451a30c50",
+    "url": "/CoronaTracker/static/js/2.bbdff6c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5293c636580b7c1f82d",
+    "url": "/CoronaTracker/static/js/main.c565e651.chunk.js"
+  },
+  {
+    "revision": "597c64ed36e2afa5fb37",
+    "url": "/CoronaTracker/static/js/runtime-main.c2155eb9.js"
+  },
+  {
+    "revision": "e5a2ad75b819a5dfefeddb05151d4b0c",
+    "url": "/CoronaTracker/static/media/covid19.e5a2ad75.jpg"
+  },
+  {
+    "revision": "7f263f203d2a38809ca18e8307e2c710",
+    "url": "/CoronaTracker/static/media/india.7f263f20.jpg"
+  }
+]);
